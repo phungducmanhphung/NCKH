6173bae58e898d8ac21e4b476e60b58e9ad9a704
vantohuu~ <vantohuu1234567@gmail.com>
2024-03-02 10:28:07 +0700

    data: dang 2/3/2024

--- Arduino/MPU6050-plotter/MPU6050-plotter.ino
+++ Arduino/MPU6050-plotter/MPU6050-plotter.ino
@@ -8,10 +8,7 @@
 
 // khỏi tao bien để lay dl tu MPU6050
 Adafruit_MPU6050 mpu;
-const int soundSensorPin = 15; // Đầu vào của cảm biến âm thanh được kết nối với chân analog A0
-
-
-
+// const int soundSensorPin = 15; // Đầu vào của cảm biến âm thanh được kết nối với chân analog A0
 
 void setup(void) {
   Serial.begin(115200);
@@ -19,11 +16,11 @@ void setup(void) {
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
   }
 
-  Wire.begin(16, 0);
+  // Wire.begin(16, 0);
 
   // Try to initialize!
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
+    // Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
     }
@@ -32,8 +29,8 @@ void setup(void) {
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.println("");
-  pinMode(soundSensorPin, INPUT); // Thiết lập chân đầu vào của cảm biến âm thanh
+  // Serial.println("");
+  // pinMode(soundSensorPin, INPUT); // Thiết lập chân đầu vào của cảm biến âm thanh
   delay(100);
 }
 
@@ -41,32 +38,31 @@ void loop() {
 
   /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
-  int soundValue = analogRead(soundSensorPin); // Đọc giá trị từ cảm biến âm thanh
+  // int soundValue = analogRead(soundSensorPin); // Đọc giá trị từ cảm biến âm thanh
   mpu.getEvent(&a, &g, &temp);
 
   /* Print out the values */
-  Serial.print("AccelX:");
+  // Serial.print("AccelX:");
   Serial.print(a.acceleration.x);
   Serial.print(",");
-  Serial.print("AccelY:");
+  // Serial.print("AccelY:");
   Serial.print(a.acceleration.y);
   Serial.print(",");
-  Serial.print("AccelZ:");
+  // Serial.print("AccelZ:");
   Serial.print(a.acceleration.z);
-  Serial.print(", ");
-  Serial.print("GyroX:");
+  Serial.print(",");
+  // Serial.print("GyroX:");
   Serial.print(g.gyro.x);
   Serial.print(",");
-  Serial.print("GyroY:");
+  // Serial.print("GyroY:");
   Serial.print(g.gyro.y);
   Serial.print(",");
-  Serial.print("GyroZ:");
+  // Serial.print("GyroZ:");
   Serial.print(g.gyro.z);
-  Serial.println(",");
- 
-  
-  Serial.print("Sound:");
-  Serial.println(soundValue); // In giá trị âm thanh lên Serial Monitor
   Serial.println("");
-  delay(10);
+ 
+  // Serial.print("Sound:");
+  // Serial.println(soundValue); // In giá trị âm thanh lên Serial Monitor
+  // Serial.println("");
+  delay(32);
 }
